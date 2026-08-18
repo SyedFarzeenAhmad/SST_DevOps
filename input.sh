0adda02a7feb9b1172f83c8f5d22295c0afbe269
SyedFarzeenAhmad <syedfarzeen18@gmail.com>
2026-08-18 11:30:00 +0000

    3rd shell script

---
+++ input.sh
@@ -0,0 +1,8 @@
+read -p "Enter name: " name
+read -p "Enter roll_np: " roll_no
+read -p "Enter comment: " comment
+
+echo "My name is $name"
+echo "My roll_no is $roll_no"
+echo "My comment is $comment"
+
