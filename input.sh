cfe48a7d49099eadfaee3b4b8b473ef0ca84cf97
SyedFarzeenAhmad <syedfarzeen18@gmail.com>
2026-08-18 11:34:32 +0000

    3rd shell script updated

--- input.sh
+++ input.sh
@@ -6,3 +6,6 @@ echo "My name is $name"
 echo "My roll_no is $roll_no"
 echo "My comment is $comment"
 
+# -s is for hiding user input
+# -n 'number' limits the input size
+# -t 'number' is for timeout count
